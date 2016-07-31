e202e0c6c92429daedf37a26d2f8984c43cd160a
Adrian Booth <adrian.booth@yourgolftravel.com>
2016-07-31 16:53:20 +0100

    finish basic set up for models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  # root to: "home#index"
+  root 'chat_rooms#index'
+
   devise_for :users
+  
+  resources :chat_rooms, only: [:new, :create, :show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
